4b4d142786a2ccf5faa36d9d5b016ece9417f368
Marc Brolly <marcrobertjohn@gmail.com>
2023-06-07 17:08:13 +0100

    Add basic foooter  HTML and CSS

--- Client/src/components/Footer.js
+++ Client/src/components/Footer.js
@@ -2,8 +2,38 @@ import "../CSS/Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer-container">
-      <p>Footer</p>
-    </div>
+    <footer className="footer-container">
+      <div className="footer-row">
+        <div className="footer-col">
+          <h4>Useful Link</h4>
+          <ul>
+            <li>One</li>
+            <li>One</li>
+            <li>One</li>
+            <li>One</li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Useful Link</h4>
+          <ul>
+            <li>One</li>
+            <li>One</li>
+            <li>One</li>
+            <li>One</li>
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h4>Useful Link</h4>
+          <ul>
+            <li>One</li>
+            <li>One</li>
+            <li>One</li>
+            <li>One</li>
+          </ul>
+        </div>
+      </div>
+      <hr className="footer-hr" />
+      <h4 className="footer-end">JobSearch.com Ltd © 2023</h4>
+    </footer>
   );
 }
